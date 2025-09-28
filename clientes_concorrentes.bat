189b2c2b759c345fc3cfa09aeb19c96bacd99138
Andrei <andreirech22@outlook.com>
2025-09-28 12:03:14 -0300

    feat: add batch script to initiate multiple test clients for file transfer

---
+++ clientes_concorrentes.bat
@@ -0,0 +1,10 @@
+@echo off
+
+ECHO Iniciando 4 clientes de teste...
+
+START "Cliente 1" cmd /k "(echo put ClientFiles/arquivo200MB.bin & echo quit) | python client.py 127.0.0.1 23456"
+START "Cliente 2" cmd /k "(echo put ClientFiles/arquivo200MB.bin & echo quit) | python client.py 127.0.0.1 23456"
+START "Cliente 3" cmd /k "(echo put ClientFiles/arquivo200MB.bin & echo quit) | python client.py 127.0.0.1 23456"
+START "Cliente 4" cmd /k "(echo put ClientFiles/arquivo200MB.bin & echo quit) | python client.py 127.0.0.1 23456"
+
+ECHO Clientes iniciados. Eles irao fechar ao concluir a transferencia.
